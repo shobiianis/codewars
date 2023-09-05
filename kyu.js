a7eda576858a04f10bb9fd63668fd8f5a9245ee6
shobii anis <shoaibwork2000@gmail.com>
2023-09-05 08:05:59 -0700

    sc

--- kyu.js
+++ kyu.js
@@ -165,4 +165,103 @@ const divisions = (n, divisor) => {
     console.log(divisions(5,5))
     console.log(divisions(21312231,4234))
     
-// ============================================================================    
+// https://www.codewars.com/kata/54d1c59aba326343c80000e7
+
+const solve = (x, y) => x/y
+
+// https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
+
+function removeChar(str){
+  strArr=str.split('')
+   strArr.splice(0,1)
+   strArr.splice(-1)
+  return  strArr.join('')};
+ 
+ 
+  // https://www.codewars.com/kata/574b1916a3ebd6e4fa0012e7
+  
+  function isOpposite(s1,s2){
+    if(s1.length===s2.length && s1.length>0 && s2.length>0){
+    for(let i=0;i<s1.length;i++){
+     if( s1[i]===s2[i] ){
+        return false
+        break;
+     }  
+    }
+    return true;
+  }
+  
+  else{
+    return false
+  }
+    
+  }
+
+  // https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec
+  function persistence(num) {
+    count=0;
+    return persistency(num)   
+}
+    
+    function persistency(num){
+      
+    if(num.toString().length>1){
+      
+        numArr=num.toString().split('')
+        val=numArr.reduce((accum,curr)=> accum*curr ) 
+    
+        count++
+       if(val.toString().length !== 1 ){  
+        return persistency(val) }
+      else {
+       return count    
+      }
+     
+     
+    }
+    else {return 0}
+    
+    }
+
+    // https://www.codewars.com/kata/5868812b15f0057e05000001
+    function tailSwap(arr){
+      resultArr=[]
+      return tailSwaps(arr)  
+      }
+      function tailSwaps(arr) {
+         let arr1=[]
+        arr.forEach((item,i)=>{
+          
+          // console.log(item.split(':')[0])
+              // console.log(item.split(':')[1])
+              arr1.push(item.split(':')[0])
+              arr1.push(item.split(':')[1])
+        })
+        resultArr.push(arr1[0]+":"+arr1[3])
+        resultArr.push(arr1[2]+":"+arr1[1])
+        return resultArr
+      }
+
+
+// https://www.codewars.com/kata/62c93765cef6f10030dfa92b
+
+function solution(start, finish,step=0) 
+{ 
+  
+    while(start!=finish){
+    if((start+3)>finish)
+  {
+      start=start+1;
+      step++
+  }else{
+      start=start+3
+      step++
+}
+
+
+}
+return step
+}
+
+
+console.log(solution(1,5))
